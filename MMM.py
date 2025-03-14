76c28b6121688c19ebf47e088a253b1a3afe0e81
JordanMRDass <89823009+JordanMRDass@users.noreply.github.com>
2025-03-14 18:53:28 +0800

    Update MMM.py

--- MMM.py
+++ MMM.py
@@ -210,7 +210,7 @@ if uploaded_file is not None:
         # Rename columns
         df_shift_all_bad.rename(columns={"Issue": "Count of Tickets"}, inplace=True)
 
-        groupby_month = df_shift_all_bad[["Month - Year", "Process", "Count of Tickets"]].groupby(by = ["Month - Year", "Process"]).count().reset_index()
+        groupby_month = df_shift_all_bad[["Month - Year", "Process", "Count of Tickets"]].groupby(by = ["Month - Year", "Process"]).count().reset_index().set_index("Month - Year")
 
         # Display the DataFrame grouping by the new 'Month' column and the 'Process' column
         st.dataframe(groupby_month, use_container_width=True)
